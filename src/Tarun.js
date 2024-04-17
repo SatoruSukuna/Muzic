fb0a1a27af73c52c06e2c920638b74efe7868b6e
Tarun <tarunsagwal38@gmail.com>
2024-04-17 12:31:22 +0530

    abc

---
+++ src/Tarun.js
@@ -0,0 +1,15 @@
+import React from "react";
+import App from "./App";
+
+export default function tarun(){
+    return(
+        <>
+        <div className="tarun">
+            <h1 className="tarun__title">Tarun</h1>
+            <h2 className="tarun__subtitle">Software Engineer</h2>
+            <h3 className="tarun__description">I am a software engineer with a passion for learning new technologies and developing software.</h3>
+        </div>
+        <App />
+        </>
+    )
+}
